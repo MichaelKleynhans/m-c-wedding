b3e11357130f9159b60badfc28221663f6c0bfa7
Carolyne Wamono <113364285+carolyne-web@users.noreply.github.com>
2025-08-13 13:27:58 +0000

    added tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"], // adjust to match your project structure
+  theme: {
+    extend: {
+      colors: {
+        sage: '#c3d69b', // your custom color
+      },
+    },
+  },
+  plugins: [],
+}
